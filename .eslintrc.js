0357576f915efa492d72a81c1cc13f8c2d678a7d
yash bansod <yashbansodgit@gmail.com>
2024-02-09 01:20:31 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+module.exports = {
+  extends: [
+    "@vercel/style-guide/eslint/node",
+    "@vercel/style-guide/eslint/typescript",
+    "@vercel/style-guide/eslint/browser",
+    "@vercel/style-guide/eslint/react",
+    "@vercel/style-guide/eslint/next",
+    "eslint-config-turbo",
+  ].map(require.resolve),
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  plugins: ["only-warn"],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ["node_modules/", "dist/"],
+  rules: {
+    "import/no-default-export": "off",
+  },
+};
